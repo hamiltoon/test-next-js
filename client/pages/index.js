e6e5f33f994380644c2ff9f34df47fe51b3261c7
John Alm Mårtensson <john.alm.martensson@gmail.com>
2022-09-03 17:56:08 +0000

    adds graphql base

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,48 @@
 import Head from 'next/head'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-export default function Home() {
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql/',
+    cache: new InMemoryCache()
+  });
+
+  const { data } = await client.query({
+    query: gql`
+    query GetLaunches {
+      launchesPast(limit: 10) {
+        id
+        mission_name
+        launch_date_local
+        launch_site {
+          site_name_long
+        }
+        links {
+          article_link
+          video_link
+          mission_patch
+        }
+        rocket {
+          rocket_name
+        }
+      }
+    }
+  `
+  });
+
+  return {
+    props: {
+      launches: data.launchesPast
+    }
+  }
+}
+
+
+export default function Home({ launches }) {
+
+  console.log('launches', launches);
+
   return (
     <div className="container">
       <Head>
@@ -9,43 +51,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        {launches.map(launch => {
+          return (
+            <a key={launch.id} href={launch.links.video_link} >
+              <h3>{launch.mission_name}</h3>
+              <p><strong>Launch Date:</strong> {new Date(launch.launch_date_local).toLocaleDateString("en-US")}</p>
+            </a>
+          );
+        })}
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer>
